5068b94948924ff3f0ea0a22c3685207434ee625
Ruslan K <ruslan.kuyanets@yandex.ru>
2024-02-11 17:02:34 +0300

    add news page and react router

--- src/entities/news/index.ts
+++ src/entities/news/index.ts
@@ -1,5 +1,6 @@
 import { NewsApiResponse, INews } from "./model/types";
 import NewsCard from "./ui/NewsCard/NewsCard";
+import NewsDetails from "./ui/NewsDetails/NewsDetails";
 
 export type { NewsApiResponse, INews };
-export { NewsCard };
+export { NewsCard, NewsDetails };
--- src/entities/news/ui/NewsCard/NewsCard.tsx
+++ src/entities/news/ui/NewsCard/NewsCard.tsx
@@ -2,13 +2,15 @@ import { formatTimeAgo } from "@/shared/helpers/formatTimeAgo";
 import { INews } from "../..";
 import styles from "./styles.module.css";
 import Image from "@/shared/ui/Image/Image";
+import { ReactNode } from "react";
 
 interface Props {
   item: INews;
   type: "banner" | "item";
+  viewNewsSlot?: (news: INews) => ReactNode;
 }
 
-const NewsCard = ({ item, type = "item" }: Props) => {
+const NewsCard = ({ item, type = "item", viewNewsSlot }: Props) => {
   return (
     <li className={`${styles.card} ${type === "banner" && styles.banner}`}>
       {type === "banner" ? (
@@ -26,6 +28,8 @@ const NewsCard = ({ item, type = "item" }: Props) => {
           {formatTimeAgo(item.published)} by {item.author}
         </p>
       </div>
+
+      {viewNewsSlot ? viewNewsSlot(item) : null}
     </li>
   );
 };
